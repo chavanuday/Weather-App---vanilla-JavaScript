0a74e37ee4ba52db4a76f98ba8bc96ecf1a56dfa
Uday Chavan Git <udaymchavan228@gmail.com>
2023-07-07 18:27:31 +0530

    updated forcast async file and app js file with Ui

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,1 +1,31 @@
-const key = `40a7c022375d22a7d657b04c8a03b852`;
+const key = `n3VJe5GtOVhh2flGFBi3520fMGB9B1ln`;
+
+// get weather info
+
+const getweather = async (id) => {
+  const base = "http://dataservice.accuweather.com/currentconditions/v1/";
+  const query = `${id}?apikey=${key}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+};
+
+const getCity = async (city) => {
+  const base = `http://dataservice.accuweather.com/locations/v1/cities/search`;
+  const query = `?apikey=${key}&q=${city}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+  return data[0];
+};
+
+getCity("kalyan")
+  .then((data) => {
+    return getweather(data.Key);
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => console.log(err));
